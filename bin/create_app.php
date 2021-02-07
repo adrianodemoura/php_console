a281c4413a3f1c9ac7bddda6e76be66350d107bc
Adriano Moura <adrianodemoura@gmail.com>
2021-02-07 14:32:18 -0300

    incluindo diretório de exemplo

--- bin/create_app.php
+++ bin/create_app.php
@@ -4,28 +4,27 @@
 define( 'DS', DIRECTORY_SEPARATOR );
 define( 'APP', str_replace( ['/Utility', '/src', '/bin', '/adrianodemoura', '/vendor', '/php_console'], '', __DIR__ ) );
 
-// create temporary directory
-exec("mkdir -p ".APP."/tmp");
-exec("touch ".APP."/tmp/empty");
-exec("echo \"tmp/*\n!tmp/empty\n\" > " . APP . "/.gitignore");
-
-// create a simple example
-exec("mkdir -p ".APP."/src/Console/Exemplo");
-exec("touch ".APP."/src/Console/Exemplo/Exemplo.php");
-$textoExemplo = "<?php
-declare(strict_types=1);
-
-namespace App\Console\Exemplo;
-use PhpConsole\Console\Console;
-
-class Exemplo extends Console {
-
-	public function execute()
-	{
-		gravaLog( $ this->getParams(), 'parametros' );
-
-		success('veja o resultado do script no diretório tmp');
-	}
+if ( is_dir(APP."/src/Console") )
+{
+	throw new Exception( "O Projeto já foi iniciado seu animal !");
 }
-";
-exec("echo \"$textoExemplo\" > " . APP . '/src/Console/Exemplo/Exemplo.php');
+
+// copy gitignore
+exec("cp ".APP."/vendor/adrianodemoura/php_console/examples/project/.gitignore ".APP);
+
+// create exemple console
+exec("mkdir -p ".APP."/src/Console/Exemplo");
+exec("cp ".APP."/vendor/adrianodemoura/php_console/examples/project/Exemplo.php ".APP."/src/Console/Exemplo");
+
+// copy composer project
+exec("cp ".APP."/vendor/adrianodemoura/php_console/examples/project/composer.json ".APP);
+
+// update composer
+$user 		= exec("whoami");
+$dirBase 	= explode( DS, exec("pwd") ); 
+$dirBase 	= end( $dirBase );
+exec("sed -i 's/seu_usuario/$user/g' ".APP."/composer.json");
+exec("sed -i 's/seu_projeto/$dirBase/g' ".APP."/composer.json");
+
+// atualizando autoload
+exec("composer dump");
